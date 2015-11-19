54070ca40542a029a95785368495dd21b4b1eed7
Paul Sullivan <Psul87@gmail.com>
2015-11-19 18:02:14 -0500

    2 characters (robot, eye man) done

---
+++ New Unity Project 1/flappy goat/Assets/fences.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fences : MonoBehaviour {
+
+	public class Obstacle : MonoBehaviour
+	{
+		public Vector2 velocity = new Vector2(-4, 0);
+		
+		// Use this for initialization
+		void Start()
+		{
+			GetComponent<Rigidbody2D>().velocity = velocity;
+		}
+	}
+}
