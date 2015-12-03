69174be6b34716b8eb3493abd03b62f76e0aa598
Claire Hu <hum085@newschool.edu>
2015-12-03 17:13:40 -0500

    I-P interaction scripts

--- Rave Bonanza/Assets/Script/playerController.cs
+++ Rave Bonanza/Assets/Script/playerController.cs
@@ -4,45 +4,48 @@ using System.Collections;
 public class playerController : MonoBehaviour
 {
 
-	private float speed;
-	private float RotationSpeed = 1.2f;
+	public float playerSpeed;
+	private float rotateSpeed = 1.2f;
 	private float xSpeed;
 	private float ySpeed;
 	public float jumpSpeed = 8.0f;
 	public float gravity = 20.0f;
-	private Vector3 moveDirection = Vector3.zero;
+//	private Vector3 moveDirection = Vector3.zero;
 	
 	void Start ()
 	{
 		
-		speed = 25f;
+		playerSpeed = 25f;
 		
 	}
 	
 	void Update ()
 	{
-		xSpeed = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
-		ySpeed = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+		xSpeed = Input.GetAxis ("Horizontal") * playerSpeed * Time.deltaTime;
+		ySpeed = Input.GetAxis ("Vertical") * playerSpeed * Time.deltaTime;
+
+		jumpSpeed = Input.GetAxis ("Jump") * jumpSpeed * Time.deltaTime;
 		
 		transform.Translate (Vector3.forward * xSpeed);
 		transform.Translate (Vector3.left * ySpeed);
+
+		transform.Translate (Vector3.up * jumpSpeed);
 		
 		
-		transform.Rotate (0, (Input.GetAxis ("Mouse X") * RotationSpeed), 0, Space.World);
-		
-		//jump
+		transform.Rotate (0, (Input.GetAxis ("Mouse X") * rotateSpeed), 0, Space.World);
+
 
 		//CharacterController controller = GetComponent<CharacterController>();
 		//if (controller.isGrounded) {
 			//moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 			//moveDirection = transform.TransformDirection(moveDirection);
-			//moveDirection *= speed;
+			//moveDirection *= playerSpeed;
 //		if (Input.GetKey(KeyCode.Space)) {
 //			
-//			transform.position += Vector3.up * speed * Time.deltaTime;
+//			transform.position += Vector3.up * playerSpeed * Time.deltaTime;
 //		} else{
 //			
-//			transform.position += Vector3.down * speed  * Time.deltaTime;
+//			transform.position += Vector3.down * playerSpeed  * Time.deltaTime;
 //			
 //		}
 		
