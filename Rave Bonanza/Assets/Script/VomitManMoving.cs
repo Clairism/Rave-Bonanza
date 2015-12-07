06633253777d7423e454c74cfe2f30371a535d07
Claire Hu <hum085@newschool.edu>
2015-12-07 02:28:54 -0500

    Random movement script & new prefabs

--- Rave Bonanza/Assets/Script/VomitManMoving.cs
+++ Rave Bonanza/Assets/Script/VomitManMoving.cs
@@ -14,16 +14,28 @@ public class VomitManMoving : folksMoving {
 
 		speedChange = 20f;
 
+		Wander ();
+
 	}
 
 	public override void Update () {
 
+		transform.position += transform.TransformDirection(Vector3.forward) * speed *Time.deltaTime;
+		
+		if((transform.position - wayPoint).magnitude < 1)
+		{
+			// when the distance between us and the target is less than 3
+			// create a new way point target
+			Wander();
+			
+		}
+
 		if(vomiting == true){
 			//slow down speed
 			GameObject.FindGameObjectWithTag("Player").GetComponent<playerController>().playerSpeed -= speedChange;
-
+			
 			Invoke ("SpeedBack", 8f);
-
+			
 			//vomit particle effect
 			vomiting = false;
 		}
--- Rave Bonanza/Assets/Script/folksMoving.cs
+++ Rave Bonanza/Assets/Script/folksMoving.cs
@@ -4,13 +4,50 @@ using System.Collections;
 public class folksMoving : MonoBehaviour {
 
 	public float speed;
-	public float moveDirection;
+
+	public Vector3 wayPoint;
+
+//	public float xMax;
+//	public float zMax;
+//	public float xMin;
+//	public float zMin;
+	public float Range;
+	//private float z;
+//	private float timeGap;
+	public float direction;
 
 	public virtual void Start () {
+
 		speed = 15f;
+
+		Wander();
+
+
 	}
 	
 	public virtual void Update () {
+
+		transform.position += transform.TransformDirection(Vector3.forward) * speed *Time.deltaTime;
+
+			if((transform.position - wayPoint).magnitude < 1)
+			{
+				// when the distance between us and the target is less than 3
+				// create a new way point target
+				Wander();
+
+			}
+}
+		
+		public void Wander()
+		{ 
+			// does nothing except pick a new destination to go to
+			
+			//wayPoint =  new Vector3(Random.Range(transform.position.x - Range, transform.position.x + Range), 1, Random.Range(transform.position.z - Range, transform.position.z + Range));
+			wayPoint= transform.localPosition + Random.insideUnitSphere * 30;
+			wayPoint.y = 1;
+			// don't need to change direction every frame seeing as you walk in a straight line only
+			transform.LookAt(wayPoint);
+			Debug.Log(wayPoint + " and " + (transform.position - wayPoint).magnitude);
+		}
 	
-	}
 }
